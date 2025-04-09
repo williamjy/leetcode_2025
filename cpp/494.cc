40a5ea1ea7f85c2871ae0c73cd52d6fab653731e
William Jiang <williamjy1018@gmail.com>
2025-04-09 20:30:33 +1000

    add 494

---
+++ cpp/494.cc
@@ -0,0 +1,19 @@
+class Solution {
+    public:
+        int findTargetSumWays(vector<int>& nums, int target) {
+            vector<vector<int>> dp(nums.size(), vector<int>(2001, 0));
+            dp[0][nums[0] + 1000]++;
+            dp[0][-nums[0] + 1000]++;
+            for (int i = 1; i < nums.size(); i++) {
+                for (int j = 0; j < 2001; j++) {
+                    if (j - nums[i] >= 0 && j - nums[i] <= 2000) {
+                        dp[i][j] += dp[i - 1][j - nums[i]];
+                    }
+                    if (j + nums[i] >= 0 && j + nums[i] <= 2000) {
+                        dp[i][j] += dp[i - 1][j + nums[i]];
+                    }
+                }
+            }
+            return dp[nums.size() - 1][target + 1000];
+        }
+    };
