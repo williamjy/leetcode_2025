2eff1e9d24850eb915b4d4f63c863c7d1b346a2f
William Jiang <williamjy1018@gmail.com>
2025-07-15 12:33:29 +1000

    add 162

---
+++ cpp/162.cc
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        if (nums.size() == 1) {
+            return 0;
+        }
+        if (nums[0] > nums[1]) {
+            return 0;
+        }
+        int size = nums.size();
+        if (nums[size - 1] > nums[size - 2]) {
+            return size - 1;
+        }
+        for (int i = 1; i < size - 1; i++) {
+            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+};
